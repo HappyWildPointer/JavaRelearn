e573bf15ecffd80e029c013fe5282a9a2210c318
njq <Njqwangyi@163.com>
2021-08-13 15:29:23 +0800

    second commit

--- src/com/package1/demo.java
+++ src/com/package1/demo.java
@@ -1,25 +1,7 @@
 package com.package1;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-
 public class demo {
-    public static void main(String[] args) throws IOException {
-        String path = "src/com/package1/demo.txt";
-        File file = new File(path);
-        FileWriter writer = new FileWriter(file);
-        for(int i=0; i<10; i++){
-            writer.write("000"+i+"\n");
-        }
-        for(int i=10; i<99; i++){
-            writer.write("00"+i+"\n");
-        }
-        for(int i=100; i<999; i++){
-            writer.write("0"+i+"\n");
-        }
-        for(int i=1000; i<10000; i++){
-            writer.write(+i+"\n");
-        }
+    public static void main(String[] args){
+        System.out.println("hello world");
     }
 }
